c917083864b5fbf5d655444bfbd50dee088e0385
Daniyarsick <dan.annanurov@gmail.com>
2024-02-09 14:55:42 +0300

    Add portfolio website files

---
+++ script.js
@@ -0,0 +1,16 @@
+function openTab(evt, disciplineName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(disciplineName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Открываем первую вкладку по умолчанию
+document.getElementsByClassName("tablink")[0].click();
